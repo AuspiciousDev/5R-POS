b7820299daf57959f1193f7ca5f3e8bc869fd9b4
Auspicious <106650462+AuspiciousDev@users.noreply.github.com>
2022-12-18 03:38:35 +0800

    Login and Auth Flow and Page routes
    Added
    Login
    Refresh
    Auth
    Persist
    
    Public Routes
    Admin Routes
    
    init Employee Routes
    
    System Themes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import { ColorModeContext, useMode } from "./themes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+// * PUBLIC ROUTES
+import Login from "./pages/public/Login";
+import ForgotPassword from "./pages/public/ForgotPassword";
+import ResetPassword from "./pages/public/ResetPassword";
+import Unauthorized from "./pages/public/Unauthorized";
+import NotFound404 from "./pages/public/NotFound404";
 
+// * PRIVATE ROUTES
+import PersistLogin from "./pages/components/PersistLogin";
+import RequireAuth from "./pages/components/RequireAuth";
+
+// * ADMIN ROUTES
+import AdminLayout from "./pages/admin/AdminLayout";
+import Dashboard from "./pages/admin/Dashboard";
+import Cashier from "./pages/admin/Cashier";
+import Inventory from "./pages/admin/Inventory";
+import Employee from "./pages/admin/Employee/Employee";
+import EmployeeAdd from "./pages/admin/Employee/EmployeeAdd";
+
+import Restock from "./pages/admin/Restock";
+import Sales from "./pages/admin/Sales";
+import Archived from "./pages/admin/Archived";
+
+// * EMPLOYEE ROUTES
+import EmployeeLayout from "./pages/employee/EmployeeLayout";
+import EmpDashboard from "./pages/employee/EmpDashboard";
+EmployeeLayout;
+const USER_TYPE = {
+  ADMIN: "admin",
+  EMP: "employee",
+};
+
+const App = () => {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route
+              path="/auth/reset-password/:resetToken"
+              element={<ResetPassword />}
+            />
+            <Route path="unauthorized" element={<Unauthorized />} />
+            <Route path="*" element={<NotFound404 />} />
+            <Route element={<PersistLogin />}>
+              <Route element={<RequireAuth allowedRoles={[USER_TYPE.ADMIN]} />}>
+                <Route path="/admin" element={<AdminLayout />}>
+                  <Route index element={<Dashboard />} />
+                  <Route path="cashier" element={<Cashier />} />
+                  <Route path="inventory" element={<Inventory />} />
+                  <Route path="employee" element={<Employee />} />
+                  <Route path="employee/add" element={<EmployeeAdd />} />
+                  <Route path="restock" element={<Restock />} />
+                  <Route path="sales" element={<Sales />} />
+                  <Route path="archived" element={<Archived />} />
+                </Route>
+              </Route>
+              <Route element={<RequireAuth allowedRoles={[USER_TYPE.EMP]} />}>
+                <Route path="/employee" element={<EmployeeLayout />}>
+                  <Route index element={<EmpDashboard />} />
+                </Route>
+              </Route>
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
 
-export default App
+export default App;
