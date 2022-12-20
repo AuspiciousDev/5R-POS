5dc8ad7ec7eb7d833cbfff507e319ad8d6e15ec3
Auspicious <106650462+AuspiciousDev@users.noreply.github.com>
2022-12-20 15:48:32 +0800

    origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:5173/",
   "http://five-r-grocery.online",
   "https://fiver-store.onrender.com",
+  "https://five-r-grocery.online/",
 ];
 
 module.exports = allowedOrigins;
